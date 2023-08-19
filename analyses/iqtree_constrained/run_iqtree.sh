800dd0d6ed5f12d9dddcb772f060a6fad32bfde4
bdemedeiros <bdemedeiros@fieldmuseum.org>
2023-08-19 12:41:39 -0500

    iqtree without rogues finished

---
+++ analyses/iqtree_constrained/run_iqtree.sh
@@ -0,0 +1,3 @@
+iqtree -s concatenated_alignment.nex -m MFP+MERGE -B 1000 -p partitions.nex -g partial_iqtree_pruned.tre -T 3 --prefix Diptera_ML_withoutrogues
+#iqtree -s concatenated_alignment.nex -m MFP+MERGE -B 1000 -p partitions.nex -T AUTO --prefix Diptera_ML
+
