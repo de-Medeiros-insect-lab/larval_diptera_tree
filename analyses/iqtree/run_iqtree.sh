8061e1cc294ef1a2d635d6586e45ddeb9d89f291
bdemedeiros <bdemedeiros@fieldmuseum.org>
2023-11-27 14:16:21 -0600

    IQTREE unconstrained tree

--- analyses/iqtree/run_iqtree.sh
+++ analyses/iqtree/run_iqtree.sh
@@ -1,4 +1,4 @@
-#iqtree -s concatenated_alignment.nex -m MFP+MERGE -B 1000 -p partitions.nex -T 5 -mset mrbayes --prefix Diptera_ML_unconstrained
-iqtree -T 5 -s concatenated_alignment.nex -mset mrbayes -m MFP+MERGE -B 1000 -p partitions_simplified.nex -g partial_iqtree.tre -T AUTO --prefix Diptera_ML
+iqtree -T 5 -s concatenated_alignment.nex -mset mrbayes -m MFP+MERGE -B 1000 -p partitions.nex -g partial_iqtree.tre --prefix Diptera_ML
+iqtree -T 5 -s concatenated_alignment.nex -mset mrbayes -m MFP+MERGE -B 1000 -p partitions.nex --prefix Diptera_ML_unconstrained
 
 
